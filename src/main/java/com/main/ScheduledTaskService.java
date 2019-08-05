be4bf69e2fbb19ce9c80d25ebf7536c67b150bb6
俊広 塩月 <shiotsuki@worldmarksolutions.com>
2019-08-05 14:26:08 +0900

    監視時間の変更

--- src/main/java/com/main/ScheduledTaskService.java
+++ src/main/java/com/main/ScheduledTaskService.java
@@ -31,7 +31,7 @@ public class ScheduledTaskService
     /**
      * プッシュメッセージスケジュール
      */
-    @Scheduled(initialDelay=0, fixedDelay=70000)
+    @Scheduled(initialDelay=0, fixedDelay=90000)
     public void executeAlarm()
     {
     	Resource resource = null;
