ccf99df607e44221f0d1e356645c1a6b3d7c940f
俊広 塩月 <shiotsuki@worldmarksolutions.com>
2020-07-24 15:30:45 +0900

    とりあえず

--- src/main/java/com/main/LineBotController.java
+++ src/main/java/com/main/LineBotController.java
@@ -8,8 +8,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LineBotController {
 
+//	@Autowired
+//	private LineMessagingClient lineMessagingClient;
+
 	@RequestMapping(value = "/linebot")
-	String index(HttpServletRequest request) throws RuntimeException {
-		return "OK";
+	void index(HttpServletRequest request) throws RuntimeException {
+
+/*
+		System.out.println("request: " + request.getParameter("textt").toString());
+
+		@SuppressWarnings("unused")
+		BotApiResponse response;
+		try {
+			response = this.lineMessagingClient
+			        .pushMessage(new PushMessage("Ud6e699869888126beedab30c5b3d484e".toString(),
+			                     new TextMessage(request.getParameter("text").toString()
+			                      ))).get();
+
+		} catch (InterruptedException | ExecutionException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+*/
 	}
 }
